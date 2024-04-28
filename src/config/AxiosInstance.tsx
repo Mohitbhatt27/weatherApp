60ffb52a99fb8c27f58e9f7dad83143fdd894a5c
Mohit <mohit.peak@gmail.com>
2024-04-28 16:27:00 +0530

    Added axios instance

---
+++ src/config/AxiosInstance.tsx
@@ -0,0 +1,8 @@
+import axios, { AxiosInstance } from "axios";
+
+export const axiosInstance: AxiosInstance = axios.create({
+  baseURL: "http://api.weatherapi.com/v1",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
