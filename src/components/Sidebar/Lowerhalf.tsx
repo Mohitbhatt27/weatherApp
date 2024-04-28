520dfe0f3f2c2bbfd222abeceb16b9df60b1e606
Mohit <mohit.peak@gmail.com>
2024-04-29 00:59:50 +0530

    upper half sidebar

---
+++ src/components/Sidebar/Lowerhalf.tsx
@@ -0,0 +1,8 @@
+function Lowerhalf() {
+  return <>
+  <div className="divider" style={{color: "white"}}></div>
+  
+  </>;
+}
+
+export default Lowerhalf;
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,11 @@
+import Upperhalf from "./Upperhalf";
+import Lowerhalf from "../Sidebar/Lowerhalf";
+
 function Sidebar() {
   return (
       <div className="basis-3/12 flex flex-col items-center text-black h-full rounded-tl-3xl rounded-bl-3xl" style={{background: 'rgba(255,255,255,0.815'}}>
-          Sidebar
+          <Upperhalf/>
+          <Lowerhalf/>
       </div>
   )
 }
