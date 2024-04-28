fb1ea77fac4fbd63433c506eb3ddeed1c837f577
Mohit <mohit.peak@gmail.com>
2024-04-29 02:14:26 +0530

    completed ui changes

--- src/components/MainData/HighlightRow.tsx
+++ src/components/MainData/HighlightRow.tsx
@@ -1,5 +1,16 @@
-function HighlightRow() {
-  return <div>Highlight Row</div>;
+import HighlightCard from "../HighlightCards";
+
+function HighLightRow() {
+  return (
+    <div className="flex flex-wrap gap-6 px-12 py-2">
+      <HighlightCard title="UV Index" data="3.8" footer="Low" />
+      <HighlightCard title="UV Index" data="3.8" footer="Low" />
+      <HighlightCard title="UV Index" data="3.8" footer="Low" />
+      <HighlightCard title="UV Index" data="3.8" footer="Low" />
+      <HighlightCard title="UV Index" data="3.8" footer="Low" />
+      <HighlightCard title="UV Index" data="3.8" footer="Low" />
+    </div>
+  );
 }
 
-export default HighlightRow;
+export default HighLightRow;
